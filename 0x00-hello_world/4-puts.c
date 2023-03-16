84903ee01f902f738ac957a795aa843d08caa996
root <Mehdi>
2023-03-16 14:50:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main(void){
 /**
  * main -main block
  * Return:0
  */
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
