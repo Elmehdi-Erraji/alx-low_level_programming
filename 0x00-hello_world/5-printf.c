9159fa039a7d282b7b79e9e3f333ff29eacff477
root <Mehdi>
2023-03-16 15:56:06 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main -A program that print a line using the printf f unction
- * Return: 0 (Succes)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
-~        
