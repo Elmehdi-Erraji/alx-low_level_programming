6c92e261414ee4f24bf120e2d0e1bea750d80246
root <Mehdi>
2023-03-21 13:03:57 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print _putchar, followed by a new line.
+ * Return: 0
+ **/
+
+int main(void)
+{
+	unsigned int i ;
+	char c[] = "_putchar";
+
+	for (i=0;i<=7;i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+
+
+}
