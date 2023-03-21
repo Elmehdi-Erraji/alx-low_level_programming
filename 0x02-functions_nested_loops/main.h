bf24bec1b1ba58efffb5b732675bdc30e5b095e1
root <Mehdi>
2023-03-21 13:18:43 -0700

    main file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_tible(void);
+
+#endif /* _MAIN_H_ */
