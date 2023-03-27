de1b9476858156318e55847d7d5d625d6b5a7951
Mehdi <elmahdi20erraji@gmail.com>
2023-03-27 16:02:00 -0700

    advanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random passwords for 101-crackme
+ * Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
