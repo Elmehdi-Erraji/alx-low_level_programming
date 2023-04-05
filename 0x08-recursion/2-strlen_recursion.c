f7b06504644f8f568bf968c756753cb9d6513b04
Mehdi <elmahdi20erraji@gmail.com>
2023-04-05 09:47:27 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,14 @@
-}
+#include "main.h"
+/**
+ * _strlen_recursion - Main Entry
+ * @s: input
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}}
