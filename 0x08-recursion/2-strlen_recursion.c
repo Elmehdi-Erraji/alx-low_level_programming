a2f59b9223562a430c84c1111f624e22a740ed70
Mehdi <elmahdi20erraji@gmail.com>
2023-04-05 09:46:00 -0700

    fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,1 @@
-#include "main.h"
-/**
- * _print_rev_recursion - Main Entry
- * @s: input
- */
-void _print_rev_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
 }
