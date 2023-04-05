844f6a3499f2e44df7e7887abe16e2f0e6ca7326
Mehdi <elmahdi20erraji@gmail.com>
2023-04-05 09:48:13 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - Main Entry
+ * @n: input
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
