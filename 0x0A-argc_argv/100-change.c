b11ab015d08d1ca5333587ddfaaed835328ab702
Mehdi <elmahdi20erraji@gmail.com>
2023-04-08 17:34:03 -0700

    advanced task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 (success)
+**/
+int main(int argc, char *argv[])
+{
+int cents, coins = 0;
+
+if (argc == 2)
+{
+cents = atoi(*(argv + 1));
+while (cents > 0)
+{
+if (cents % 25 < cents)
+{
+cents -= 25;
+coins++;
+}
+else if (cents % 10 < cents)
+{
+cents -= 10;
+coins++;
+}
+else if (cents % 5 < cents)
+{
+cents -= 5;
+coins++;
+}
+else if (cents % 2 < cents)
+{
+cents -= 2;
+coins++;			}
+else if (cents % 1 < cents)
+{
+cents -= 1;
+coins++;
+}
+}
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", coins);
+return (0);
+}
