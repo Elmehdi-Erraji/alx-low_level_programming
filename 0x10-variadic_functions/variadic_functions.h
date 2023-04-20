ab9737a5201bc4896f3c3f15653250f93ecdeba2
Mehdi <elmahdi20erraji@gmail.com>
2023-04-20 15:46:35 -0700

    tasks finished

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * File: main
+ *
+ * Desc: header file
+**/
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
