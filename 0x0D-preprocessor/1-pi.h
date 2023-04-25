32a0ba37ff342b45137a9079c650b52cee993b2b
Mehdi <elmahdi20erraji@gmail.com>
2023-04-25 18:24:34 +0100

    tasks done

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: 1-pi
+ *
+ * Desc: header file
+ */
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+/**
+ * File: 3-function_like_macro
+ * @X: input
+ * Desc: header file
+ */
+#define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
+
+#endif
